afd678d238c1bdde33c647dcdfbcf113eafb5334
marcfdzz <marc.fernandez@acceleralia.com>
2026-02-02 01:36:00 +0100

    gfd

--- anguar/src/app/app.routes.ts
+++ anguar/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home.component';
 import { AprendreComponent } from './aprendre.component';
 import { RecursosComponent } from './recursos.component';
 import { SobreComponent } from './sobre.component';
+import { ContaminacioComponent } from './contaminacio.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'aprendre', component: AprendreComponent },
+  { path: 'contaminacio', component: ContaminacioComponent },
   { path: 'recursos', component: RecursosComponent },
   { path: 'sobre', component: SobreComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
